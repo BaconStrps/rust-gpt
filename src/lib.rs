e30b5131fbc8b006d868a8c6d3f4ed1b00134053
Jared Cochran <88454324+BaconStrps@users.noreply.github.com>
2023-03-08 01:52:22 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,293 @@
+#![allow(dead_code)]
+// this library is super unfinished
+// and incredibly unpolished
+// but it'll work for borf for now
+use serde_json::json;
+use serde::Serialize;
+use once_cell::sync::OnceCell;
+
+static RQCLIENT: OnceCell<reqwest::Client> = OnceCell::new();
+static COMPLETION_URL: &'static str = "https://api.openai.com/v1/completions";
+static CHAT_URL: &'static str = "https://api.openai.com/v1/chat/completions";
+
+pub trait CompletionLike {}
+trait SendRequest {
+    fn send(self) -> Result<CompletionResponse, reqwest::Error>;
+}
+pub struct Completion;
+pub struct Chat;
+#[derive(Debug)]
+pub enum CompletionModel{
+    TextDavinci003,
+    TextDavinci002,
+    CodeDavinci002,
+}
+
+pub enum ChatModel {
+    Gpt35Turbo,
+    GPT35Turbo0301,
+}
+
+impl CompletionLike for Completion {}
+impl CompletionLike for Chat {}
+
+impl ToString for CompletionModel {
+    fn to_string(&self) -> String {
+        match self {
+            CompletionModel::TextDavinci003 => "text-davinci-003",
+            CompletionModel::TextDavinci002 => "text-davinci-002",
+            CompletionModel::CodeDavinci002 => "code-davinci-002",
+        }.to_string()
+    }
+}
+
+impl ToString for ChatModel {
+    fn to_string(&self) -> String {
+        match self {
+            ChatModel::Gpt35Turbo => "gpt-3.5-turbo",
+            ChatModel::GPT35Turbo0301 => "gpt-3.5-turbo-0301",
+        }.to_string()
+    }
+}
+
+#[derive(Debug)]
+pub struct CompletionChoice {
+    text: String,
+    index: u32,
+    logprobs: u32,
+    finish_reason: String,
+}
+
+impl From<serde_json::Value> for CompletionChoice {
+    fn from(value: serde_json::Value) -> Self {
+        Self {
+            text: value["text"].as_str().unwrap().to_string(),
+            index: value["index"].as_u64().unwrap() as u32,
+            logprobs: value["logprobs"].as_u64().unwrap_or_default() as u32,
+            finish_reason: value["finish_reason"].as_str().unwrap_or_default().to_string(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct CompletionResponse {
+    id: String,
+    object: String,
+    created: u64,
+    model: String,
+    choices: Vec<CompletionChoice>,
+}
+
+#[derive(Debug)]
+pub struct ChatResponse {
+    id: String,
+    object: String,
+    created: u64,
+    model: String,
+    usage: (u32, u32, u32),
+    messages: Vec<ChatMessage>,
+}
+
+
+
+#[derive(Debug, Serialize)]
+pub struct ChatMessage {
+    pub role: String,
+    pub content: String,
+}
+
+impl Into<serde_json::Value> for ChatMessage {
+    fn into(self) -> serde_json::Value {
+        json!({
+            "role": self.role,
+            "content": self.content
+        })
+    }
+}
+
+impl ToString for ChatMessage {
+    fn to_string(&self) -> String {
+        json!({
+            "role": self.role,
+            "content": self.content
+        }).to_string()
+    }
+}
+
+#[derive(Debug)]
+pub struct Request<T> {
+    to_send: String,
+    api_key: String,
+    state: std::marker::PhantomData<T>,
+}
+
+impl Request<Completion> {
+
+    pub async fn send_completion(self) -> Result<CompletionResponse, reqwest::Error> {
+        let client = RQCLIENT.get_or_init(|| reqwest::Client::new());
+
+        let resp = client.post(COMPLETION_URL)
+            .header("Content-Type", "application/json")
+            .header("Authorization", self.api_key)
+            .body(self.to_send)
+            .send()
+            .await?;
+
+        let body = resp.text().await.unwrap();
+        let json: serde_json::Value = serde_json::from_str(&body).unwrap();
+        Ok(CompletionResponse {
+            id: json["id"].as_str().unwrap().to_string(),
+            object: json["object"].as_str().unwrap().to_string(),
+            created: json["created"].as_u64().unwrap(),
+            model: json["model"].as_str().unwrap().to_string(),
+            choices: json["choices"].as_array().unwrap().iter().map(|choice| CompletionChoice::from(choice.clone())).collect()
+        })
+    }
+}
+
+impl Request<Chat> {
+    pub async fn send_chat(self) -> Result<ChatResponse, reqwest::Error> {
+        let client = RQCLIENT.get_or_init(|| reqwest::Client::new());
+
+        let resp = client.post(CHAT_URL)
+            .header("Content-Type", "application/json")
+            .header("Authorization", self.api_key)
+            .body(self.to_send)
+            .send()
+            .await?;
+
+        let body = resp.text().await.unwrap();
+        let json: serde_json::Value = serde_json::from_str(&body).unwrap();
+        Ok(ChatResponse {
+            id: json["id"].as_str().unwrap().to_string(),
+            object: json["object"].as_str().unwrap().to_string(),
+            created: json["created"].as_u64().unwrap(),
+            model: json["model"].as_str().unwrap().to_string(),
+            usage: (
+                json["usage"]["prompt_tokens"].as_u64().unwrap() as u32,
+                json["usage"]["completion_tokens"].as_u64().unwrap() as u32,
+                json["usage"]["total_tokens"].as_u64().unwrap() as u32,
+            ),
+            messages: json["choices"].as_array().unwrap().iter().map(|choice| ChatMessage {
+                role: choice["message"]["role"].as_str().unwrap().to_string(),
+                content: choice["message"]["content"].as_str().unwrap().to_string(),
+            }).collect()
+        })
+    }
+}
+
+
+#[derive(Debug)]
+pub struct RequestBuilder<T> {
+    req: serde_json::Value,
+    api_key: String,
+    state: std::marker::PhantomData<T>,
+}
+
+impl<C: CompletionLike> RequestBuilder<C> {
+    pub fn new<T: ToString>(model: T, mut api_key: String) -> Self {
+
+        api_key.insert_str(0, "Bearer ");
+
+        let req = json!({
+            "model": model.to_string(),
+        });
+
+        Self {
+            req,
+            api_key,
+            state: std::marker::PhantomData,
+        }
+    }
+
+    pub fn max_tokens(mut self, max_tokens: u32) -> Self {
+        self.req["max_tokens"] = json!(max_tokens);
+        self
+    }
+
+    pub fn temperature(mut self, temperature: f32) -> Self {
+        self.req["temperature"] = json!(temperature);
+        self
+    }
+
+    pub fn top_p(mut self, top_p: f32) -> Self {
+        self.req["top_p"] = json!(top_p);
+        self
+    }
+
+    pub fn frequency_penalty(mut self, frequency_penalty: f32) -> Self {
+        self.req["frequency_penalty"] = json!(frequency_penalty);
+        self
+    }
+
+    pub fn presence_penalty(mut self, presence_penalty: f32) -> Self {
+        self.req["presence_penalty"] = json!(presence_penalty);
+        self
+    }
+
+    pub fn stop<T: ToString>(mut self, stop: T) -> Self {
+        self.req["stop"] = json!(stop.to_string());
+        self
+    }
+
+    pub fn n(mut self, n: u32) -> Self {
+        self.req["n"] = json!(n);
+        self
+    }
+
+    pub fn stream(mut self, stream: bool) -> Self {
+        self.req["stream"] = json!(stream);
+        self
+    }
+
+    pub fn logprobs(mut self, logprobs: u32) -> Self {
+        self.req["logprobs"] = json!(logprobs);
+        self
+    }
+
+    pub fn echo(mut self, echo: bool) -> Self {
+        self.req["echo"] = json!(echo);
+        self
+    }
+
+    pub fn best_of(mut self, best_of: u32) -> Self {
+        self.req["best_of"] = json!(best_of);
+        self
+    }
+
+    pub fn logit_bias(mut self, logit_bias: serde_json::Value) -> Self {
+        self.req["logit_bias"] = logit_bias;
+        self
+    }
+}
+
+impl RequestBuilder<Completion> {
+    pub fn prompt<T: ToString>(mut self, prompt: T) -> Self {
+        self.req["prompt"] = json!(prompt.to_string());
+        self
+    }
+
+    pub fn build_completion(self) -> Request<Completion> {
+        Request {
+            api_key: self.api_key,
+            to_send: self.req.to_string(),
+            state: std::marker::PhantomData
+        }
+    }
+}
+
+impl RequestBuilder<Chat> {
+
+    pub fn messages(mut self, messages: Vec<ChatMessage>) -> Self {
+        self.req["messages"] = json!(messages);
+        self
+    }
+
+    pub fn build_chat(self) -> Request<Chat> {
+        Request {
+            api_key: self.api_key,
+            to_send: self.req.to_string(),
+            state: std::marker::PhantomData
+        }
+    }
+}
---
+++ tests/example_request.rs
@@ -0,0 +1,43 @@
+
+use rust_gpt::*;
+
+#[test]
+fn example_chat_request() {
+    let req = RequestBuilder::new(ChatModel::Gpt35Turbo, std::env::var("OPENAI_API_KEY").unwrap())
+        .messages(vec![ChatMessage{
+            role: "system".to_string(),
+            content: "You are a helpful assistant.".to_string()
+        }, ChatMessage{
+            role: "user".to_string(),
+            content: "Who started World War 2?".to_string()
+        }])
+        .max_tokens(128)
+        .build_chat();
+
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let resphandle = rt.block_on(req.send_chat()).unwrap();
+
+    println!("Chat: {:?}", resphandle);
+}
+
+
+#[test]
+fn example_completion_request() {
+    let req = RequestBuilder::new(CompletionModel::TextDavinci003, std::env::var("OPENAI_API_KEY").unwrap())
+        .prompt("Once upon a time, there was a")
+        .max_tokens(128)
+        .build_completion();
+
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let resphandle = rt.block_on(req.send_completion()).unwrap();
+
+    println!("Completion: {:?}", resphandle);
+}
